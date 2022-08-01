28379c312a8a246f478dc4abad0cc57726df47c7
cameroncircowestfalltechnik <107223062+cameroncircowestfalltechnik@users.noreply.github.com>
2022-08-01 15:39:28 -0700

    Update and rename Main_Emulated_Startup.py to Main_Startup.py

--- Main_Startup.py
+++ Main_Startup.py
@@ -4,9 +4,9 @@ import os
 #So, this program is run instead, as it closes the old program first then relaunches it. Finally, this program is terminated on main script start so neither the main or startup scripts leak memory or open multiple instances.
 
 try: #try the following
-    os.system("\n pkill -f Main_Emulated.py") #kill the main program (ie. kill it if it is still running)
+    os.system("\n pkill -f Main.py") #kill the main program (ie. kill it if it is still running)
 except:
     pass
 os.system("cd /home/pi/Desktop") #mavigate to program location
-os.system("\n sudo python Main_Emulated.py") #start the program
+os.system("\n sudo python Main.py") #start the program
 
